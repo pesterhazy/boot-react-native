0cd765e7f3ccff685e471a6d6f0e7e0d27ca5d53
Paulus Esterhazy <pesterhazy@gmail.com>
2016-04-13 12:19:08 +0200

    Add more demo content

--- example/src/mattsum/simple_example/core.cljs
+++ example/src/mattsum/simple_example/core.cljs
@@ -11,6 +11,8 @@
 
 (def view (reag/adapt-react-class (.-View react)))
 (def text (reag/adapt-react-class (.-Text react)))
+(def image (reag/adapt-react-class (.-Image react)))
+(def input (reag/adapt-react-class (.-TextInput react)))
 
 (defn testf
   []
@@ -19,8 +21,24 @@
 
 (defn root-view
   []
-  [view
-   [text {:style {:margin-top 22, :margin-left 8}} "CHANGE THIS: HELLO WORLD"]])
+  [view {:style {:margin-top 22
+                 :margin-left 8
+                 :margin-right 8}}
+   [text {:style {:font-family "futura"
+                  :font-size 24
+                  :text-align "center"
+                  }}
+    "Clojure Berlin Demo"]
+   #_[view {:style {:background-color :red :height 100}}]
+   #_[image {:style {:width 380 :height 380}
+             :source {:uri "http://photos1.meetupstatic.com/photos/event/1/e/3/6/highres_438427734.jpeg"}}]
+   #_[input {:style {:height 40 :width 300
+                     :margin-left 20}
+             :on-change-text (fn [v] (prn v))
+             :background-color "pink"
+             :placeholder "potatoes"
+             :default-value "potatoes"}]
+   ])
 
 (defn mount-root []
   (reag/render [root-view] 1))
